213b1e9e35a7c15bb6b75ed25d2e0147bdc3419c
Adamu Suleiman Gbale <124920139+samweb1122@users.noreply.github.com>
2025-07-22 05:34:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -478,3 +478,23 @@ window.addEventListener('DOMContentLoaded', () => {
     audio.play();
   });
 });
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) =>
+      cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json'
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
